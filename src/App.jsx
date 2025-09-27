9fea02b1f550f261f25510e5046fe80b4b268fc6
sidak32 <sidak2k4@gmail.com>
2025-09-27 18:29:00 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div>
-      <h1>My CRM App</h1>
-      <Outlet /> {/* this is where child routes like homepage.jsx will render */}
+      <Outlet />{" "}
+      {/* this is where child routes like homepage.jsx will render */}
     </div>
   );
 }
